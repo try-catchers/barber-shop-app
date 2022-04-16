5e746d67e1ccd88c33da3cb7cffce8148ca665b2
Enzo Falone <enzofalone123@gmail.com>
2022-04-16 15:17:01 -0400

    Initial commit

---
+++ app/src/main/java/com/efalone/barbershop/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package com.efalone.barbershop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+
+    //TODO: Go into main fragment after user registers successfully
+}
